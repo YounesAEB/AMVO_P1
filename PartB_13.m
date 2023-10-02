81e9a41d3bcf1d80edbb7cb7e8f7e2e6e9e60628
PolRuiz <pol.ruiz.celada@estudiantat.upc.edu>
2023-10-02 10:17:21 +0200

    Clean code

--- PartB_13.m
+++ PartB_13.m
@@ -7,25 +7,36 @@
 clc; clear; close all;
 
 % Input parameters
-N  = 3;
-L  = 1;                % domain size
+N  = 3; 
+L  = 1; % domain size
 
-up = zeros(N+2,N+2);
-up(2,3) = 1; % imbalance
-vp = zeros(N+2,N+2);
-vp = haloUpdate(vp);
-up = haloUpdate(up);
+
+% Impose vectorial fields
+up      = zeros(N+2,N+2);
+up(2,3) = 1; % Introduce imbalance
+vp      = zeros(N+2,N+2);
+vp      = haloUpdate(vp);
+up      = haloUpdate(up);
+
+
+% Discretization of the Poisson equation for the pseudo-p
 [d] = diverg(up,vp,L);
 [b] = field2vector(d);
 [A] = computeMatrixA(N);
-p=A\b;
+p   = A\b;
 [pseudo] = vector2field(p);
-pseudo = haloUpdate(pseudo);
+pseudo   = haloUpdate(pseudo);
+
+% Gradient of the pseudo-p
 [gx,gy] = grad(pseudo,L);
 gx = haloUpdate(gx);
 gy = haloUpdate(gy);
+
+% Updated velocity fields
 un = up - gx;
 un = haloUpdate(un);
 vn = vp - gy;
 vn = haloUpdate(vn);
+
+% Proof
 [proof] = diverg(un,vn,L);
